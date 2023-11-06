5a6a328da7e3cf897ff817acff1cf0e2e6425007
Valentin Degrange <valentin.degrange@gmail.com>
2023-11-06 16:43:05 +0100

    Remove test on notifications - handle by the manager

--- pyPlants/tests/models/test_notifications.py
+++ pyPlants/tests/models/test_notifications.py
@@ -1,6 +1,5 @@
 from django.test import TestCase, tag
 from django.utils import timezone
-from freezegun import freeze_time
 
 from pyPlants.models import PlantUser, NotificationCenter, Notification
 
@@ -25,27 +24,3 @@ class NotificationTest(TestCase):
 
     def test_no_notification_created(self):
         self.assertEqual(Notification.objects.count(), 0)
-
-    @freeze_time('2023-01-01')
-    def test_notification_created(self):
-        # proper notification sent
-        notification = self.notification_center_1.send_notification('test')
-        self.assertEqual(Notification.objects.count(), 1)
-        self.assertEqual(notification.user, self.user_1)
-        self.assertEqual(notification.message, 'test')
-        self.assertEqual(self.notification_center_1.last_notification_sent, timezone.now())
-        # user 2 don't get notifications
-        self.assertIsNone(self.notification_center_2.last_notification_sent)
-        self.assertFalse(Notification.objects.filter(user=self.user_2).exists())
-
-    def test_notification_viewed(self):
-        notification = self.notification_center_1.send_notification('test')
-        self.assertFalse(notification.viewed)
-        notification.mark_as_viewed()
-        self.assertTrue(notification.viewed)
-        self.assertIsNotNone(notification.viewed_at)
-
-    def test_notification_not_viewed(self):
-        notification = self.notification_center_1.send_notification('test')
-        self.assertFalse(notification.viewed)
-        self.assertIsNone(notification.viewed_at)
