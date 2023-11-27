5bc94014715c53facbca9a117860446478fda231
Valentin Degrange <valentin.degrange@gmail.com>
2023-11-27 18:05:29 +0100

    Properly thumbnail images if available

--- pyPlants/models.py
+++ pyPlants/models.py
@@ -152,11 +152,12 @@ class Plant(AbstractPlantModel):
 
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
-        img = Image.open(self.image.path)
-        if img.height > 512 or img.width > 512:
-            output_size = (512, 512)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
+        if self.image:
+            img = Image.open(self.image.path)
+            if img.height > 512 or img.width > 512:
+                output_size = (512, 512)
+                img.thumbnail(output_size)
+                img.save(self.image.path)
 
     def __str__(self):
         return self.name
